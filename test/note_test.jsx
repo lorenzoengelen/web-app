e82adf840caaa0383995bb4177a3c286e5087975
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-25 16:55:50 -0700

    created a 'test' test file

---
+++ test/note_test.jsx
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import chai, { expect } from 'chai';
+
+describe('App jsx test', () => {
+  it('this is a failing test', () => {
+    expect(5).to.be.a('string');
+  });
+});
