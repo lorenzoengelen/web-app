c21d0edd4884c24df6d95164c6dd916ede799720
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-25 15:58:49 -0700

    created karma configuration file

---
+++ karma.config.js
@@ -0,0 +1,40 @@
+module.exports = function karmaConfig(config) {
+  config.set({
+    frameworks: [
+      // mocha testing framework
+      'mocha'
+    ],
+    reporters: [
+      // set reporter to print detailed results to console
+      'spec',
+      // output code coverage files
+      'coverage'
+    ],
+    files: [
+      // React.js requires bind and phantomjs does not support it
+      'node_modules/phantomjs-polyfill/bind-polyfill.js',
+      // grab all files in the test directory that contain _test.
+      'test/**/*_test.*'
+    ],
+    preprocessors: {
+      // convert files with webpack and load sourcemaps
+      'test/**/*_test.*': ['webpack', 'sourcemap']
+    },
+    browsers: [
+      // run tests using PhantomJS
+      'PhantomJS'
+    ],
+    singleRun: true,
+    // configure coverage reporter
+    coverageReporter: {
+      dir: 'build/coverage/',
+      type: 'html'
+    },
+    // test webpack config
+    webpack: require('./webpack.config'),
+    // hide webpack build information from output 
+    webpackMiddleware: {
+      noInfo: true
+    }
+  });
+};
