eb8a413abc63659d6d22f0be44b12977344bc89f
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-02 14:38:55 -0700

    created a catalog file

---
+++ app/components/Catalog.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Catalog extends Component {
+  render() {
+    return (
+      <div>This is the Catalog</div>
+    );
+  }
+}
+
+export default Catalog;
