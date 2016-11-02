984a01f9d34344e0bf3672ac537d86ebfb702aae
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-02 12:38:35 -0700

    updated header jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: []
+    };
+  }
+
   render() {
-    return ();
+    return (
+      <div>
+        <div className='test'>This is the header</div>
+        <span className='child'></span>
+      </div>
+    );
   }
 }
 
