e5b6e5bd2649c88b64fbf197f574d90a01ea8107
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-02 16:07:27 -0700

    refactored tab list items

--- app/components/Catalog.jsx
+++ app/components/Catalog.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 
 class Catalog extends Component {
+  renderCatalogItems() {
+    const categories = [
+      'Nieuw',
+      'Banken',
+      'Stoelen',
+      'Tafels',
+      'Bedden',
+      'Kasten',
+      'Verlichting',
+      'Decoratie',
+      'Kantoor',
+      'Kinderen'
+    ];
+
+    return categories.map(category => {
+      return <li className='catalog-item' role='presentation'><a href='#'>{category}</a></li>;
+    });
+  }
+
   render() {
     return (
       <div className='container'>
         <ul className='nav nav-tabs nav-justified catalog-items'>
-          <li className='catalog-item' role='presentation'><a href='#'>Nieuw</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Banken</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Stoelen</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Tafels</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Bedden</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Kasten</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Verlichting</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Decoratie</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Kantoor</a></li>
-          <li className='catalog-item' role='presentation'><a href='#'>Kinderen</a></li>
+          {this.renderCatalogItems()}
         </ul>
       </div>
     );
