413fc47da5a02871a6c8f74f93cae6571bc0c387
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-02 14:55:27 -0700

    wrapped header and catalog in a div

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import Header from './Header.jsx';
+import Catalog from './Catalog.jsx';
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div className='application'>
+        <Header />
+        <Catalog />
+      </div>
     );
   }
 }
