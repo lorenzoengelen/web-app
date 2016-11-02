d99fbe4791e3b1e511822ca379c39d70b2a8eecf
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-02 15:14:05 -0700

    basic tests for the catalog

---
+++ test/app_test/components_test/Catalog_test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import chai, {expect} from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import {render, shallow} from 'enzyme';
+
+chai.use(chaiEnzyme());
+
+import Catalog from '../../../app/components/Catalog.jsx';
+
+describe('<Catalog />', () => {
+
+  it('has sofas "banken"', () => {
+
+  });
+
+  it('has chairs "stoelen"', () => {
+
+  });
+
+  it('has tables "tafels"', () => {
+
+  });
+
+  it('has beds "bedden"', () => {
+
+  });
+
+  it('has storage "kasten"', () => {
+
+  });
+
+  it('has lighting "verlichting"', () => {
+
+  });
+
+  it('has decoration "decoratie"', () => {
+
+  });
+
+  it('has office "kantoor"', () => {
+
+  });
+
+  it('has children furniture "kinderen"', () => {
+
+  });
+
+});
