3318161793b972a2e43fd1bc6d8fa271958101d8
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-02 15:52:12 -0700

    created a nav tab with catalog items

--- app/components/Catalog.jsx
+++ app/components/Catalog.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class Catalog extends Component {
   render() {
     return (
-      <div>This is the Catalog</div>
+      <div className='container'>
+        <ul className='nav nav-tabs nav-justified catalog-items'>
+          <li className='catalog-item' role='presentation'><a href='#'>Nieuw</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Banken</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Stoelen</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Tafels</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Bedden</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Kasten</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Verlichting</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Decoratie</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Kantoor</a></li>
+          <li className='catalog-item' role='presentation'><a href='#'>Kinderen</a></li>
+        </ul>
+      </div>
     );
   }
 }
