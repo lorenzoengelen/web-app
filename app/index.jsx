9b65f8524c21d604e826059d7f5390b3fedfd795
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-03 11:24:00 -0700

    updated react route

--- app/index.jsx
+++ app/index.jsx
@@ -4,11 +4,17 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // components
 import App from './components/App.jsx';
+import Shop from './components/shop/Shop.jsx';
+import NoMatch from './components/NoMatch.jsx';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path='/' component={App}>
       <IndexRoute />
+      <Route path='shop' component={Shop}>
+        <Route path='/shop/:category' component={Shop} />
+      </Route>
+      <Route path='*' component={NoMatch} />
     </Route>
   </Router>
 ), document.getElementById('app'));
