be9691ccd426881da3e038341d73d3f5f58f523c
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-03 11:59:13 -0700

    created a filter component

---
+++ app/components/shop/Filters.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Filters extends Component {
+  render() {
+    return (
+      <div className='filters'>
+        <h5>Well... here are some filters</h5>
+      </div>
+    );
+  }
+}
+
+export default Filters;
