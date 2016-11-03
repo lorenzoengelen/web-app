1c4bc36155abdc20c0bedd3033d190057bbe132b
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-03 11:32:27 -0700

    created a file for the banner on the shop page

---
+++ app/components/shop/Banner.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Banner extends Component {
+  render() {
+    return (
+      <div className='banner'>
+        <h3>This is the banner</h3>
+      </div>
+    );
+  }
+}
+
+export default Banner;
