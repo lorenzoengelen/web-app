c67ec0bc82976ae7e065be1f748ce117c6d18c91
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-03 11:24:44 -0700

    created a folder for shop and a container for shopping

---
+++ app/components/shop/Shop.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Shop extends Component {
+  render() {
+    return (
+      <div className='shop'>The shop page is under construction</div>
+    );
+  }
+}
+
+export default Shop;
