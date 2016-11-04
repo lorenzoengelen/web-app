224a5d78a3f16804d5e271fd16299c9116cd5151
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-04 14:46:33 -0700

    banner renders category name

--- app/components/shop/Banner.jsx
+++ app/components/shop/Banner.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
+const bannerStyle = {
+  backgroundImage: 'url(' + require('../../assets/img/office.jpg') + ')',
+  backgroundSize: 'cover',
+  // backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+  minHeight: '200px',
+  color: 'white'
+  // maxHeight: '100%'
+};
+
 class Banner extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const category = this.props.category;
     return (
-      <div className='banner'>
-        <h3>This is the banner</h3>
-        <img src={require('../../assets/img/office.jpg')} className='img-responsive' alt='Responsive image'/>
+      <div className='banner' style={bannerStyle}>
+        <h1 className='text-center banner-header'>{category}</h1>
       </div>
     );
   }
