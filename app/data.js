e21d15fe0b9a9ddc9adf76bb9beb29db02746c9d
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-03 17:14:10 -0700

    added mock data (categories and subcategories)

---
+++ app/data.js
@@ -0,0 +1,443 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'sofas',
+    en: 'Sofas',
+    nl: 'Banken',
+    parentId: 1
+  },
+  {
+    id: 101,
+    name: '',
+    en: '',
+    nl: 'Hoekbanken',
+    parentId: 1
+  },
+  {
+    id: 102,
+    name: '',
+    en: '',
+    nl: 'Tweezitsbanken',
+    parentId: 1
+  },
+  {
+    id: 103,
+    name: '',
+    en: '',
+    nl: 'Driezitsbanken',
+    parentId: 1
+  },
+  {
+    id: 104,
+    name: '',
+    en: '',
+    nl: 'Chaises',
+    parentId: 1
+  },
+  {
+    id: 105,
+    name: '',
+    en: '',
+    nl: 'Slaapbanken',
+    parentId: 1
+  },
+  {
+    id: 106,
+    name: '',
+    en: '',
+    nl: 'Eetkamerbanken',
+    parentId: 1
+  },
+  {
+    id: 2,
+    name: '',
+    en: '',
+    nl: 'Stoelen',
+    parentId: 2
+  },
+  {
+    id: 201,
+    name: '',
+    en: '',
+    nl: 'Armstoelen',
+    parentId: 2
+  },
+  {
+    id: 202,
+    name: '',
+    en: '',
+    nl: 'Eetkamerstoelen',
+    parentId: 2
+  },
+  {
+    id: 203,
+    name: '',
+    en: '',
+    nl: 'Krukken',
+    parentId: 2
+  },
+  {
+    id: 204,
+    name: '',
+    en: '',
+    nl: 'Barkrukken',
+    parentId: 2
+  },
+  {
+    id: 205,
+    name: '',
+    en: '',
+    nl: 'Bureaustoelen',
+    parentId: 2
+  },
+  {
+    id: 206,
+    name: '',
+    en: '',
+    nl: 'Poefen',
+    parentId: 2
+  },
+  {
+    id: 3,
+    name: '',
+    en: '',
+    nl: 'Tafels',
+    parentId: 3
+  },
+  {
+    id: 301,
+    name: '',
+    en: '',
+    nl: 'Salontafels',
+    parentId: 3
+  },
+  {
+    id: 302,
+    name: '',
+    en: '',
+    nl: 'Eetkamertafels',
+    parentId: 3
+  },
+  {
+    id: 303,
+    name: '',
+    en: '',
+    nl: 'Bartafels',
+    parentId: 3
+  },
+  {
+    id: 304,
+    name: '',
+    en: '',
+    nl: 'Bureaus',
+    parentId: 3
+  },
+  {
+    id: 305,
+    name: '',
+    en: '',
+    nl: 'Sidetable',
+    parentId: 3
+  },
+  {
+    id: 306,
+    name: '',
+    en: '',
+    nl: 'Bijzettafels',
+    parentId: 3
+  },
+  {
+    id: 4,
+    name: '',
+    en: '',
+    nl: 'Bedden',
+    parentId: 4
+  },
+  {
+    id: 401,
+    name: '',
+    en: '',
+    nl: 'Bedden',
+    parentId: 4
+  },
+  {
+    id: 402,
+    name: '',
+    en: '',
+    nl: 'Boxsprings',
+    parentId: 4
+  },
+  {
+    id: 403,
+    name: '',
+    en: '',
+    nl: 'Bedbanken',
+    parentId: 4
+  },
+  {
+    id: 404,
+    name: '',
+    en: '',
+    nl: 'Hoogslapers',
+    parentId: 4
+  },
+  {
+    id: 405,
+    name: '',
+    en: '',
+    nl: 'Stapelbedden',
+    parentId: 4
+  },
+  {
+    id: 406,
+    name: '',
+    en: '',
+    nl: 'Kinderbedden',
+    parentId: 4
+  },
+  {
+    id: 5,
+    name: '',
+    en: '',
+    nl: 'Kasten',
+    parentId: 5
+  },
+  {
+    id: 501,
+    name: '',
+    en: '',
+    nl: 'Ladekasten',
+    parentId: 5
+  },
+  {
+    id: 502,
+    name: '',
+    en: '',
+    nl: 'Schoenenkasten',
+    parentId: 5
+  },
+  {
+    id: 503,
+    name: '',
+    en: '',
+    nl: 'Opbergkasten',
+    parentId: 5
+  },
+  {
+    id: 504,
+    name: '',
+    en: '',
+    nl: 'Boekenkasten',
+    parentId: 5
+  },
+  {
+    id: 505,
+    name: '',
+    en: '',
+    nl: 'Vitrinekasten',
+    parentId: 5
+  },
+  {
+    id: 506,
+    name: '',
+    en: '',
+    nl: 'Wandkasten',
+    parentId: 5
+  },
+  {
+    id: 507,
+    name: '',
+    en: '',
+    nl: 'Wandkasten',
+    parentId: 5
+  },
+  {
+    id: 508,
+    name: '',
+    en: '',
+    nl: 'Buffetkasten',
+    parentId: 5
+  },
+  {
+    id: 509,
+    name: '',
+    en: '',
+    nl: 'Tv Meubels',
+    parentId: 5
+  },
+  {
+    id: 510,
+    name: '',
+    en: '',
+    nl: 'Dressoirs',
+    parentId: 5
+  },
+  {
+    id: 6,
+    name: '',
+    en: '',
+    nl: 'Verlichting',
+    parentId: 6
+  },
+  {
+    id: 601,
+    name: '',
+    en: '',
+    nl: 'Kroonluchters',
+    parentId: 6
+  },
+  {
+    id: 602,
+    name: '',
+    en: '',
+    nl: 'Hanglampen',
+    parentId: 6
+  },
+  {
+    id: 603,
+    name: '',
+    en: '',
+    nl: 'Plafondlampen',
+    parentId: 6
+  },
+  {
+    id: 604,
+    name: '',
+    en: '',
+    nl: 'Tafellampen',
+    parentId: 6
+  },
+  {
+    id: 605,
+    name: '',
+    en: '',
+    nl: 'Wandlampen',
+    parentId: 6
+  },
+  {
+    id: 606,
+    name: '',
+    en: '',
+    nl: 'Vloerlampen',
+    parentId: 6
+  },
+  {
+    id: 607,
+    name: '',
+    en: '',
+    nl: 'Spots',
+    parentId: 6
+  },
+  {
+    id: 608,
+    name: '',
+    en: '',
+    nl: 'Inbouwlampen',
+    parentId: 6
+  },
+  {
+    id: 7,
+    name: '',
+    en: '',
+    nl: 'Decoratie',
+    parentId: 7
+  },
+  {
+    id: 701,
+    name: '',
+    en: '',
+    nl: 'Vloerkleden',
+    parentId: 7
+  },
+  {
+    id: 702,
+    name: '',
+    en: '',
+    nl: 'Kapstokken',
+    parentId: 7
+  },
+  {
+    id: 703,
+    name: '',
+    en: '',
+    nl: 'Krantenbakken',
+    parentId: 7
+  },
+  {
+    id: 704,
+    name: '',
+    en: '',
+    nl: 'Spiegels',
+    parentId: 7
+  },
+  {
+    id: 705,
+    name: '',
+    en: '',
+    nl: 'Boekenplanken',
+    parentId: 7
+  },
+  {
+    id: 706,
+    name: '',
+    en: 'Storage',
+    nl: 'Opbergen',
+    parentId: 7
+  },
+  {
+    id: 707,
+    name: '',
+    en: '',
+    nl: 'Woonkussen',
+    parentId: 7
+  },
+  {
+    id: 8,
+    name: '',
+    en: '',
+    nl: 'Kindermeubilair',
+    parentId: 8
+  },
+  {
+    id: 801,
+    name: '',
+    en: '',
+    nl: 'Bedden',
+    parentId: 8
+  },
+  {
+    id: 802,
+    name: '',
+    en: '',
+    nl: 'Boxen',
+    parentId: 8
+  },
+  {
+    id: 803,
+    name: '',
+    en: '',
+    nl: 'Commodes',
+    parentId: 8
+  },
+  {
+    id: 804,
+    name: '',
+    en: '',
+    nl: 'Kasten',
+    parentId: 8
+  },
+  {
+    id: 805,
+    name: '',
+    en: '',
+    nl: 'Kinderstoelen',
+    parentId: 8
+  },
+  {
+    id: 806,
+    name: '',
+    en: '',
+    nl: 'Stapelbedden',
+    parentId: 8
+  }
+];
