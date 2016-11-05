bd217910072298f5c4e253a8e26607a6cd63255c
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-05 16:51:32 -0700

    search for parent category name in database

--- app/components/shop/Banner.jsx
+++ app/components/shop/Banner.jsx
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 const bannerStyle = {
   backgroundImage: 'url(' + require('../../assets/img/office.jpg') + ')',
   backgroundSize: 'cover',
-  // backgroundPosition: 'center center',
   backgroundRepeat: 'no-repeat',
   minHeight: '200px',
   color: 'white'
-  // maxHeight: '100%'
 };
+
+import { data } from '../../data.js';
 
 class Banner extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentWillMount() {
+    this.getCategory();
+  }
+
+  componentWillUpdate() {
+    this.getCategory();
+  }
+
+  getCategory() {
+    const {category} = this.props;
+    const {parentId} = _.find(data, ({name}) => {
+      return name === category;
+    }) || {parentId: 0};
+    const {nl} = _.find(data, ({id}) => {
+      return id === parentId;
+    }) || {nl: 'Nieuw'};
+    console.log(nl);
+    return nl;
+  }
+
   render() {
-    const category = this.props.category;
+    const {category} = this.props;
     return (
       <div className='banner' style={bannerStyle}>
-        <h1 className='text-center banner-header'>{category}</h1>
+        <h1 className='text-center banner-header'>{this.getCategory()}</h1>
       </div>
     );
   }
