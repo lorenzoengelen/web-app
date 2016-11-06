6caca53740bba9379bde1baf22f91470c4b5e7af
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-05 17:38:27 -0700

    render subcategories in new nav bar

--- app/components/shop/Banner.jsx
+++ app/components/shop/Banner.jsx
@@ -26,21 +26,39 @@ class Banner extends Component {
 
   getCategory() {
     const {category} = this.props;
+    if (category === 'new') { return 'Nieuw' }
     const {parentId} = _.find(data, ({name}) => {
       return name === category;
-    }) || {parentId: 0};
+    });
     const {nl} = _.find(data, ({id}) => {
       return id === parentId;
-    }) || {nl: 'Nieuw'};
-    console.log(nl);
+    });
     return nl;
   }
 
-  render() {
+  renderSubcategories() {
     const {category} = this.props;
+    if (category === 'new') { return; }
+    const {parentId} = _.find(data, ({name}) => {
+      return name === category;
+    });
+    const subcategories = _.filter(data, (item) => {
+      return parentId === item.parentId && item.id !== item.parentId;
+    });
+    return _.map(subcategories, ({id, nl}) => {
+      return (
+        <li key={id}><a href='#'>{nl}</a></li>
+      );
+    });
+  }
+
+  render() {
     return (
       <div className='banner' style={bannerStyle}>
         <h1 className='text-center banner-header'>{this.getCategory()}</h1>
+        <ul className='nav nav-pills nav-justified'>
+          {this.renderSubcategories()}
+        </ul>
       </div>
     );
   }
