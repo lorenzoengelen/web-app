ff16d86ea04e37460941a577a27d5102487e1849
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-07 11:53:57 -0800

    setup catalog action

--- app/actions/catalog.js
+++ app/actions/catalog.js
@@ -1,1 +1,13 @@
 import catalog from '../api/catalog';
+import * as types from '../constants/ActionTypes';
+
+const receiveCatalog = categories => ({
+  type: types.RECEIVE_CATALOG,
+  categories: categories
+});
+
+export const getCatalog = () => dispatch => {
+  catalog.getCategories(categories => {
+    dispatch(receiveCatalog(categories));
+  });
+};
