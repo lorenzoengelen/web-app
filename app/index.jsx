9a5712c9a6a5f397a74d1008afec03bcc0bb31df
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-07 10:48:26 -0800

    starting redux setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // components
