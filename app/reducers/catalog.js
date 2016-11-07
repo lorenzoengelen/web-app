757c4b7988278bb4879e9023f9827390e74f29de
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-07 13:09:55 -0800

    updated reducer file for catalog, now categorize on id

--- app/reducers/catalog.js
+++ app/reducers/catalog.js
@@ -1,17 +1,35 @@
 import { combineReducers } from 'redux';
 import { RECEIVE_CATALOG } from '../constants/ActionTypes';
 
-const categories = (state = [], action) => {
+const categories = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_CATALOG:
-      return [...state, ...action.categories];
+      return {
+        ...state,
+        ...action.categories.filter(({id, parentId}) => {
+          return id === parentId;
+        }).reduce((obj, category) => {
+          obj[category.id] = category;
+          return obj;
+        }, {})
+      };
     default:
       return state;
   }
 };
 
-const subcategories = (state = [], action) => {
+const subcategories = (state = {}, action) => {
   switch (action.type) {
+    case RECEIVE_CATALOG:
+      return {
+        ...state,
+        ...action.categories.filter(({id, parentId}) => {
+          return id !== parentId;
+        }).reduce((obj, category) => {
+          obj[category.id] = category;
+          return obj;
+        }, {})
+      };
     default:
       return state;
   }
