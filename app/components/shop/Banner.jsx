3de7c51dde165a3f9331467be219f5fa0edf9a39
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-07 22:00:19 -0800

    subcategories nav make use of redux store

--- app/components/shop/Banner.jsx
+++ app/components/shop/Banner.jsx
@@ -14,56 +14,27 @@ const titleStyle = {
   lineHeight: '120px'
 };
 
-import { data } from '../../data.js';
-
 class Banner extends Component {
   constructor(props) {
     super(props);
   }
 
-  componentWillMount() {
-    this.getCategory();
-  }
-
-  componentWillUpdate() {
-    this.getCategory();
-  }
-
-  getCategory() {
-    const {category} = this.props;
-    if (category === 'new') { return 'Nieuw'; }
-    const {parentId} = _.find(data, ({name}) => {
-      return name === category;
-    });
-    const {nl} = _.find(data, ({id}) => {
-      return id === parentId;
-    });
-    return nl;
-  }
-
-  renderSubcategories() {
-    const {category} = this.props;
-    if (category === 'new') { return; }
-    const {parentId} = _.find(data, ({name}) => {
-      return name === category;
-    });
-    const subcategories = _.filter(data, (item) => {
-      return parentId === item.parentId && item.id !== item.parentId;
-    });
-    return _.map(subcategories, ({id, nl}) => {
-      return (
-        <li key={id}><a href='#'>{nl}</a></li>
-      );
+  renderSubcategories(id) {
+    const subcategories = this.props.categories[id].subcategories;
+    return _.map(subcategories, id => {
+      const subcategory = this.props.subcategories[id];
+      return <li key={subcategory.id}><a href='#'>{subcategory.nl}</a></li>;
     });
   }
 
   render() {
-    const category = this.props.categories[this.props.currentCategory.parentId].nl || '';
+    const categoryId = this.props.currentCategory.parentId;
+    const categoryName = this.props.categories[categoryId].nl;
     return (
       <div className='banner' style={bannerStyle}>
-        <h1 className='text-center banner-header' style={titleStyle}>{category}</h1>
+        <h1 className='text-center banner-header' style={titleStyle}>{categoryName}</h1>
         <ul className='nav nav-pills nav-justified'>
-          {this.renderSubcategories()}
+          {this.renderSubcategories(categoryId)}
         </ul>
       </div>
     );
